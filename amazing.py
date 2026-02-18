06e7acd18f1bd05d203cc880ae0fc9dce7940f4c
NIRVANA20-20 <walidlaiz48@gmail.com>
2026-02-18 21:24:29 +0000

    drow N/E/W/

--- amazing.py
+++ amazing.py
@@ -19,14 +19,13 @@ class Image:
         self.buffer, self.bpp, self.sl, self.format = mlx.mlx_get_data_addr(self.ptr)
 
     def put_pixel(self, x: int, y: int, color: int):
-        offset = int((y * self.sl) + (x * (self.bpp / 8)))
+        offset = int((y * self.sl) + (x * (self.bpp // 8)))
         self.buffer[offset: offset + 4] = color.to_bytes(4, "little")
         
 
 mlx = Mlx()
 mlx_ptr = Mlx.mlx_init(mlx)
 mlx_wind = Mlx.mlx_new_window(mlx, mlx_ptr, 1440, 1440, "kary00s")
-
 
 img1 = Image(mlx, mlx_ptr, int(1200), int(1200))
 for y in range(1200):
@@ -35,7 +34,7 @@ for y in range(1200):
 
 
 def put_north(cell_border: int, cell_width: int, start_h: int, start_w: int):
-    print(cell_width, start_h, start_w)
+
     for y in range(start_h, cell_border + start_h):
         for x in range(start_w, cell_width + cell_border ):
             img1.put_pixel(x, y, 0xFADDD4FF)
@@ -43,7 +42,7 @@ def put_north(cell_border: int, cell_width: int, start_h: int, start_w: int):
 def put_west(cell_border: int, cell_height: int, start_h: int, start_w: int):
 
     for x in range(start_w, cell_border + start_w):
-        for y in range(start_h, cell_height + cell_border):
+        for y in range(start_h, cell_height):
             img1.put_pixel(x, y, 0xFADDD4FF)
 
 def put_south(cell_border: int, cell_height: int, cell_width: int, start_h: int, start_w: int):
@@ -53,9 +52,8 @@ def put_south(cell_border: int, cell_height: int, cell_width: int, start_h: int,
             img1.put_pixel(x, y,  0xFADDD4FF)
 
 def put_east(cell_width: int, cell_height: int, cell_border: int, start_h: int, start_w: int):
-    
-    for x in range(start_w + cell_width, cell_width + cell_border):
-        for y in range(start_h, cell_height + cell_border):
+    for x in range(start_w - cell_border, start_w ):
+        for y in range(start_h, cell_height):
             img1.put_pixel(x, y,  0xFADDD4FF)
    
 
@@ -65,8 +63,8 @@ def put_east(cell_width: int, cell_height: int, cell_border: int, start_h: int, 
 maze = Maze(5, 5, (0,0), (29,29))
 
 grids =  maze.grid
-
 print(grids)
+
 
 cell_h = 1200 // 5
 
@@ -76,50 +74,41 @@ cell_border = 5
 start_h = 0
 start_w = 0
 
-#put_north(cell_border, cell_width, start)
-#put_south(cell_border, cell_height, cell_width, start)
-#put_east(cell_width, cell_height, cell_border, start)
 
 cell_height = cell_h
 cell_width = cell_w
-tmp = 0
 for cells in grids:
     for cell in cells:
         x, y = cell.x, cell.y
-        if y == tmp + 1:
-            put_north(cell_border, cell_width, start_h, start_w) 
-            start_h += cell_h  
-            start_w = 0
-            tmp += 1
-        put_north(cell_border, cell_width, start_h, start_w)         
-        start_w = cell_width
+        start_h = y * cell_h
+        start_w = x * cell_w
+        cell_width = cell_w * (x + 1)
+        put_north(cell_border, cell_width, start_h, start_w)
 
-        cell_width = cell_w * (x + 1) 
+for cells in grids:
+    for cell in cells:
+        x, y = cell.x, cell.y
+        start_h = y * cell_h
+        start_w = x * cell_w
         cell_height = cell_h * (y + 1)
-    
-        print("hgvsdf",cell_height)
+        put_west(cell_border, cell_height, start_h, start_w)
+
+for cells in grids:
+    for cell in cells:
+        x, y = cell.x, cell.y
+        start_h = y * cell_h
+        start_w = cell_w * (x + 1)
+        print(start_h, start_w)
+        cell_height = cell_h * (y + 1)
+        cell_width = cell_w * (x + 1)
+            
+        put_east(cell_width, cell_height, cell_border, start_h, start_w)
+
 
 
 
 mlx.mlx_put_image_to_window(mlx_ptr, mlx_wind, img1.ptr, 0, 0)
 
-"""
-for y in range(cell_border):
-    for x in range(cell_width + cell_border):
-            img1.put_pixel(x, y, 0x0FFFFFFF)
-
-for x in range(cell_border):
-    for y in range(cell_height + cell_border):
-        img1.put_pixel(x, y, 0x0FFFFFFF)
-
-for y in range(cell_height, cell_height + cell_border):
-    for x in range(cell_width + cell_border):
-        img1.put_pixel(x, y, 0x0FFFFFFF)
-
-for x in range(cell_width, cell_width + cell_border):
-    for y in range(cell_height + cell_border):
-        img1.put_pixel(x, y,  0x0FFFFFFF)
-"""
 
 """
 
